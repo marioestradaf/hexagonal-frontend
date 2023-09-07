2c70e7e49edd8f7f609528765f1126ae9899b00b
Mario Estrada <mario.estrada@sequra.es>
2023-09-07 22:02:48 +0200

    chore(auth):  refactor TS constructor shorthand

--- src/modules/auth/application/AuthUseCase.ts
+++ src/modules/auth/application/AuthUseCase.ts
@@ -2,11 +2,7 @@ import Auth from "../domain/Auth";
 import AuthRepository from "../domain/AuthRepository";
 
 export class AuthUseCase {
-  private authRepository: AuthRepository;
-
-  constructor(authRepository: AuthRepository) {
-    this.authRepository = authRepository;
-  }
+  constructor(private readonly authRepository: AuthRepository) {}
 
   public async login(username: string, password: string): Promise<Auth> {
     const user = await this.authRepository.login(username, password);
